1b5292b0e3f1240150cad8a14c090a20c44d126f
Tomáš Diblík <dibla.tomas@post.cz>
2022-09-30 20:46:01 +0200

    [Feature] Navigation functionality

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,10 +1,15 @@
-import {StyleSheet, Text, View, Button, Image} from "react-native";
+import {StackActions} from "@react-navigation/native";
+import {View, Image, TouchableWithoutFeedback} from "react-native";
 
 const Navbar = ({navigation, children}) => (
     <View>
         <View style={{flex: 1, flexDirection: "row", justifyContent: "space-between", padding: 15}}>
-            <Image source={require("../../assets/BackButtonNavbar.svg")} style={{width: 35, height: 35}} />
-            <Image source={require("../../assets/HomeButtonNavbar.svg")} style={{width: 35, height: 35}} />
+            <TouchableWithoutFeedback onPress={() => navigation.goBack()}>
+                <Image source={require("../../assets/BackButtonNavbar.svg")} style={{width: 35, height: 35}} />
+            </TouchableWithoutFeedback>
+            <TouchableWithoutFeedback onPress={() => navigation.dispatch(StackActions.popToTop())}>
+                <Image source={require("../../assets/HomeButtonNavbar.svg")} style={{width: 35, height: 35}} />
+            </TouchableWithoutFeedback>
         </View>
         <View style={{flex: 1, padding: 15}}>{children}</View>
     </View>
