e8ca705146f4b09d717062f92e0219f113ad871d
ZirixCZ <zirixcz@gmail.com>
2022-10-01 07:14:47 +0200

    [Update] Seatch Field update

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -17,13 +17,13 @@ function App() {
         <View style={{backgroundColor: "white"}}>
         <NavigationContainer>
             <Stack.Navigator screenOptions={{headerShown: false}}>
-                <Stack.Screen name="DiagnoseTypeOfIllness" component={DiagnoseTypeOfIllness} />
-                <Stack.Screen name="docsearchdiagnosis" component={DocSearchDiagnosis} />
-                <Stack.Screen name="doctorsbyfield" component={DoctorsByField} />
-                <Stack.Screen name="doctorsbyfieldinformation" component={DoctorsByFieldInformation} />
-                <Stack.Screen name="home" component={HomeScreen} />
-                <Stack.Screen name="pokus" component={PokusScreen} />
                 <Stack.Screen name="docsearchfield" component={DocSearchField} />
+                <Stack.Screen name="doctorsbyfield" component={DoctorsByField} />
+                <Stack.Screen name="docsearchdiagnosis" component={DocSearchDiagnosis} />
+                <Stack.Screen name="DiagnoseTypeOfIllness" component={DiagnoseTypeOfIllness} />
+                <Stack.Screen name="home" component={HomeScreen} />
+                <Stack.Screen name="doctorsbyfieldinformation" component={DoctorsByFieldInformation} />
+                <Stack.Screen name="pokus" component={PokusScreen} />
             </Stack.Navigator>
         </NavigationContainer>
         </View>
--- frontend/src/components/DocSearchField.tsx
+++ frontend/src/components/DocSearchField.tsx
@@ -1,24 +1,55 @@
-import {ScrollView, Text, View, StyleSheet, Image} from "react-native";
+import React, {useState} from "react";
+import {Text, TextInput, View} from "react-native";
 import Header from "./Header/Header";
 import Navbar from "./Navbar";
-import React from "react";
 import CustomDropdown from "./CustomDropdown";
 
+const gender = ["Muž", "Žena"];
 const countries = ["Egypt", "Canada", "Australia", "Ireland"];
 const DocSearchField = ({navigation}) => {
+
+    const [age, setAge] = useState(null);
+
     return (
         <Navbar navigation={navigation}>
             <Header
                 header={"Hledat lékaře \npodle oboru"}
                 subHeader="Hledejte lékaře podle zdravotnického oboru praxe."
             />
-            <CustomDropdown
-                data={countries}
-                placeholder={"Vyberte obor..."}
-                placeholderFinder={"Hledejte obor..."}
-                onSelect={(item, index) => navigation.navigate("doctorsbyfield")}
-                padding={20}
-            />
+            <View style={{display: "flex", justifyContent: "center", marginTop: "2rem"}}>
+                <View style={{display: "flex", flexDirection: "column", justifyContent: "flex-start"}}>
+                    <View style={{display: "flex", flexDirection: "column"}}>
+                        <Text style={{marginLeft: "1rem"}}>Váš věk</Text>
+                        <TextInput
+                            style={{height: 40, borderWidth: 1, margin: 12, marginLeft: "1rem", padding: 12}}
+                            onChangeText={() => setAge}
+                            value={age}
+                            maxLength={3}
+                            keyboardType='numeric'
+                        />
+                    </View>
+                    <View style={{display: "flex", flexDirection: "column", marginTop: "1rem"}}>
+                        <Text style={{marginLeft: "1rem"}}>Váše pohlaví</Text>
+                        <CustomDropdown
+                            data={gender}
+                            placeholder={"Váše pohlaví"}
+                            placeholderFinder={"Váše pohlaví"}
+                            onSelect={(item, index) => navigation.navigate("doctorsbyfield")}
+                            padding={12}
+                        />
+                    </View>
+                    <View style={{display: "flex", flexDirection: "column", marginTop: "1rem"}}>
+                        <Text style={{marginLeft: "1rem"}}>Symptomy</Text>
+                        <CustomDropdown
+                            data={gender}
+                            placeholder={"Symptomy"}
+                            placeholderFinder={"Symptomy"}
+                            onSelect={(item, index) => navigation.navigate("doctorsbyfield")}
+                            padding={12}
+                        />
+                    </View>
+                </View>
+            </View>
         </Navbar>
     );
 };
