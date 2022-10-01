70dd699b9de80010ed5d29a2fb1e1fb08f1b28ef
ZirixCZ <zirixcz@gmail.com>
2022-10-01 08:52:15 +0200

    [Update] Added logic to the symptom page

--- frontend/src/components/DocSearchField.tsx
+++ frontend/src/components/DocSearchField.tsx
@@ -1,15 +1,17 @@
 import React, {useEffect, useState} from "react";
-import {Text, TextInput, View} from "react-native";
-import {symptoms} from "../constants";
+import {Button, Pressable, StyleSheet, Text, TextInput, View} from "react-native";
+import {colorRed, symptoms} from "../constants";
 import Header from "./Header/Header";
 import Navbar from "./Navbar";
 import CustomDropdown from "./CustomDropdown";
 
-const gender = ["Muž", "Žena"];
 const DocSearchField = ({navigation}) => {
 
-    const [pickedSymptom, setPickedSymptom] = useState("aaa");
+    const [symptom, setSymptom] = useState(null);
+    const [symptom1, setSymptom1] = useState(null);
+    const [symptom2, setSymptom2] = useState(null);
     const [age, setAge] = useState(null);
+    const [gender, setGender] = useState(null);
 
     useEffect(() => {
         // fetch("http://localhost:8080/symptoms").then((res) => {
@@ -21,8 +23,8 @@ const DocSearchField = ({navigation}) => {
 
     useEffect(()=> {
         console.log('123')
-        console.log(pickedSymptom)
-    }, [pickedSymptom])
+        console.log(symptom)
+    }, [symptom])
 
     return (
         <Navbar navigation={navigation}>
@@ -30,7 +32,7 @@ const DocSearchField = ({navigation}) => {
                 header={"Hledat lékaře \npodle oboru"}
                 subHeader="Hledejte lékaře podle zdravotnického oboru praxe."
             />
-            <View style={{display: "flex", justifyContent: "center", marginTop: "2rem"}}>
+            <View style={{display: "flex", justifyContent: "space-between", marginTop: "2rem", height: "calc(100vh - 300px)"}}>
                 <View style={{display: "flex", flexDirection: "column", justifyContent: "flex-start"}}>
                     <View style={{display: "flex", flexDirection: "column"}}>
                         <Text style={{marginLeft: "1rem"}}>Váš věk</Text>
@@ -45,28 +47,54 @@ const DocSearchField = ({navigation}) => {
                     <View style={{display: "flex", flexDirection: "column", marginTop: "1rem"}}>
                         <Text style={{marginLeft: "1rem"}}>Váše pohlaví</Text>
                         <CustomDropdown
-                            data={gender}
+                            data={["Muž", "Žena"]}
                             placeholder={"Váše pohlaví"}
                             placeholderFinder={"Váše pohlaví"}
-                            onSelect={(item, index) => navigation.navigate("doctorsbyfield")}
+                            onSelect={(item, index) => setGender(item)}
                             padding={12}
                         />
                     </View>
                     <View style={{display: "flex", flexDirection: "column", marginTop: "1rem"}}>
-                        <Text style={{marginLeft: "1rem"}}>{symptoms[pickedSymptom]?.Name}</Text>
+                        <Text style={{marginLeft: "1rem"}}>Vyberte symptom</Text>
                         <CustomDropdown
                             data={symptoms}
                             placeholder={"Symptomy"}
                             placeholderFinder={"Symptomy"}
-                            onSelect={(item, index) => {setPickedSymptom(index); console.log("abcdfrantisek"); console.log(item.Name)}}
-                            // rowToTextParsing={(item, index) => item.Name}
+                            onSelect={(item, index) => setSymptom(item)}
+                            padding={12}
+                        />
+                    </View>
+                    <View style={[symptom ? {display: "flex"} : {display: "none"}, {flexDirection: "column", marginTop: "1rem"}]}>
+                        <Text style={{marginLeft: "1rem"}}>Vyberte symptom</Text>
+                        <CustomDropdown
+                            data={symptoms}
+                            placeholder={"Symptomy"}
+                            placeholderFinder={"Symptomy"}
+                            onSelect={(item, index) => setSymptom1(item)}
                             padding={12}
                         />
                     </View>
                 </View>
+                <Pressable style={[styles.button, symptom && symptom1 ? {backgroundColor: colorRed} : {backgroundColor: "#BDBDBD"}]} onPress={() => symptom && symptom1 ? navigation.navigate("doctorsbyfield") : null}>
+                    <Text style={styles.text}>Pokračovat</Text>
+                </Pressable>
             </View>
         </Navbar>
     );
 };
+
+const styles = StyleSheet.create({
+    button: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingVertical: 20,
+        paddingHorizontal: 115,
+        borderRadius: 32,
+        elevation: 3
+    },
+    text: {
+        color: "white"
+    }
+})
 
 export default DocSearchField;
